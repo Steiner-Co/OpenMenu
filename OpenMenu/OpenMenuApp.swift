07eacedf6f10859b60b77dd632852d06ae410ed6
Arunava Karmakar <arunava19karmakar@gmail.com>
2026-01-29 14:43:48 +0530

    Initial Commit

---
+++ OpenMenu/OpenMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenMenuApp.swift
+//  OpenMenu
+//
+//  Created by Arunava Karmakar on 29/01/26.
+//
+
+import SwiftUI
+
+@main
+struct OpenMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
